80c6949d7e3cc6f70f4b013fffc4777f11764292
Danton Matheus <dantoncosta03@hotmail.com>
2022-09-26 13:14:12 -0300

    :test_tube: test: cria o teste E2E para dar downvote em uma recomendação

---
+++ front-end/cypress/e2e/downvoteRecommendation.cy.js
@@ -0,0 +1,21 @@
+/* eslint-disable no-undef */
+beforeEach(() => {
+	cy.resetDatabase();
+});
+
+describe("Teste E2E - Dar upvote em uma recomendação", () => {
+	it("Deve dar um upvote com sucesso", () => {
+		cy.visit("http://localhost:3000");
+
+		cy.createRecommendation();
+
+		cy.intercept("GET", "/recommendations").as("getRecommendations");
+		cy.wait("@getRecommendations");
+
+		cy.get("[data-cy=downvoteButton]").click();
+
+		cy.get("[data-cy=score]").should(($p) => {
+			expect($p).to.contain("-1");
+		});
+	});
+});
